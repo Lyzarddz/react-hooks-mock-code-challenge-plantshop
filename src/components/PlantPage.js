ad996e7090c2d99a82fade0b6c7d3015d97841b2
Alyssa Falcione <lyzarddz@gmail.com>
2022-04-06 12:50:20 -0400

    Done

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, {useState, useEffect}  from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants]= useState([])
+  const [search, setSearch] = useState("")
+
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((resp) => resp.json())
+    .then((data) => {
+      setPlants(data)
+    }) 
+
+  }, [])
+
+  function handleAddPlant(newPlant) {
+    const updatedPlants = [...plants, newPlant]
+    setPlants(updatedPlants)
+  }
+  
+  const listToDisplay = plants.filter(plant=> plant.name.toLowerCase().includes(search.toLowerCase()))
+
+  
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant}/>
+      <Search search={search} setSearch={setSearch}/>
+      <PlantList plants={listToDisplay} />
     </main>
   );
 }
